4cda95606b02fbbb5d031e9083af59b4be1f10fb
Николай Козинский <rifdedking@gmail.com>
2026-05-29 12:20:21 +0300

    f

---
+++ src/Laba3/Lab3_MediaCatalog/Models/Movie.cs
@@ -0,0 +1,9 @@
+﻿namespace MediaCatalog.Models;
+
+public class Movie
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = string.Empty;
+    public string Director { get; set; } = string.Empty;
+    public int Year { get; set; }
+}
